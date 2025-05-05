9470317a80a1ba4bd1a11f7039cf789de69b9aa4
Rinesa <rinesabislimi28@gmail.comgit config --global --list git config --global user.email>
2025-05-05 08:30:21 +0200

    LoginForm functionality

--- src/frontend/src/components/LoginForm.jsx
+++ src/frontend/src/components/LoginForm.jsx
@@ -22,7 +22,7 @@ function LoginForm({ onSwitch, onForgot }) {
     e.preventDefault();
     setMessage("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("http://localhost:5000/api/users/login", {
         email: formData.email,
         password: formData.password,
       });
