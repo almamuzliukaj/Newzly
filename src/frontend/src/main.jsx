4bfb3111327b9b558cc5357ce0e305c9aa92e976
Alma <150298495+almam007@users.noreply.github.com>
2025-05-05 22:36:11 +0200

    frontend style

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -3,7 +3,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css';
+import './index.css';  // For global styles like background and body styles
+import './app.css';  // For other app-specific styles
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
